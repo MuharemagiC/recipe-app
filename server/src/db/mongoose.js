9475b0029aa59bc835abc97b84fc5bfa23d21d2f
MuharemagiC <emir.rggf.muharemagic.97@gmail.com>
2021-01-08 10:43:54 +0100

    Fixing DB URL

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/recipe-app",
+  process.env.MONGO_URL || "mongodb://localhost:27017/recipe-app",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
